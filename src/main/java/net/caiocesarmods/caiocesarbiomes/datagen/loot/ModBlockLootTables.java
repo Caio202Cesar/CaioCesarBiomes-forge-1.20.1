a2425532a70e1123396e7fe9110a262daea5b85a
Caio202Cesar <caiodnobre@gmail.com>
2025-02-04 15:39:39 -0300

    Adding stripped wood.

---
+++ src/main/java/net/caiocesarmods/caiocesarbiomes/datagen/loot/ModBlockLootTables.java
@@ -0,0 +1,41 @@
+package net.caiocesarmods.caiocesarbiomes.datagen.loot;
+
+import net.caiocesarmods.caiocesarbiomes.block.ModBlocks;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+
+import java.util.Set;
+
+//Ver tutorial sobre DataGen
+public class ModBlockLootTables extends BlockLootSubProvider {
+    public ModBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        this.dropSelf(ModBlocks.ARAUCARIA_LOG.get());
+        this.dropSelf(ModBlocks.ARAUCARIA_WOOD.get());
+        this.dropSelf(ModBlocks.STRIPPED_ARAUCARIA_LOG.get());
+        this.dropSelf(ModBlocks.STRIPPED_ARAUCARIA_WOOD.get());
+        this.dropSelf(ModBlocks.ARAUCARIA_PLANKS.get());
+
+        this.dropSelf(ModBlocks.NOTHOFAGUS_LOG.get());
+        this.dropSelf(ModBlocks.NOTHOFAGUS_WOOD.get());
+        this.dropSelf(ModBlocks.STRIPPED_NOTHOFAGUS_LOG.get());
+        this.dropSelf(ModBlocks.STRIPPED_NOTHOFAGUS_WOOD.get());
+        this.dropSelf(ModBlocks.NOTHOFAGUS_PLANKS.get());
+
+        this.dropSelf(ModBlocks.ITALIAN_CYPRESS_LOG.get());
+        this.dropSelf(ModBlocks.ITALIAN_CYPRESS_WOOD.get());
+        this.dropSelf(ModBlocks.STRIPPED_ITALIAN_CYPRESS_LOG.get());
+        this.dropSelf(ModBlocks.STRIPPED_ITALIAN_CYPRESS_WOOD.get());
+        this.dropSelf(ModBlocks.ITALIAN_CYPRESS_PLANKS.get());
+
+        this.dropSelf(ModBlocks.STONE_PINE_LOG.get());
+        this.dropSelf(ModBlocks.STONE_PINE_WOOD.get());
+        this.dropSelf(ModBlocks.STRIPPED_STONE_PINE_LOG.get());
+        this.dropSelf(ModBlocks.STRIPPED_STONE_PINE_WOOD.get());
+        this.dropSelf(ModBlocks.STONE_PINE_PLANKS.get());
+    }
+}
