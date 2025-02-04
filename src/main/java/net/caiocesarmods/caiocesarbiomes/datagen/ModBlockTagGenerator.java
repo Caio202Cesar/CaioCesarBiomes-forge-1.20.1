d742f4326337f7dfd55171ab721a222a97049f8e
Caio202Cesar <caiodnobre@gmail.com>
2025-02-04 14:14:58 -0300

    Porting trees

---
+++ src/main/java/net/caiocesarmods/caiocesarbiomes/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,31 @@
+package net.caiocesarmods.caiocesarbiomes.datagen;
+
+import net.caiocesarmods.caiocesarbiomes.CaioCesarBiomesMod;
+import net.caiocesarmods.caiocesarbiomes.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, CaioCesarBiomesMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        this.tag(BlockTags.LOGS_THAT_BURN)
+                .add(ModBlocks.ARAUCARIA_LOG.get())
+                .add(ModBlocks.ARAUCARIA_WOOD.get())
+                .add(ModBlocks.STRIPPED_ARAUCARIA_LOG.get())
+                .add(ModBlocks.STRIPPED_ARAUCARIA_WOOD.get());
+
+        this.tag(BlockTags.PLANKS)
+                .add(ModBlocks.ARAUCARIA_PLANKS.get());
+
+    }
+}
---
+++ src/main/java/net/caiocesarmods/caiocesarbiomes/datagen/ModItemTagGenerator.java
@@ -0,0 +1,32 @@
+package net.caiocesarmods.caiocesarbiomes.datagen;
+
+import net.caiocesarmods.caiocesarbiomes.CaioCesarBiomesMod;
+import net.caiocesarmods.caiocesarbiomes.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagGenerator extends ItemTagsProvider {
+    public ModItemTagGenerator(PackOutput p_275343_, CompletableFuture<HolderLookup.Provider> p_275729_,
+                               CompletableFuture<TagLookup<Block>> p_275322_, @Nullable ExistingFileHelper existingFileHelper) {
+        super(p_275343_, p_275729_, p_275322_, CaioCesarBiomesMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        this.tag(ItemTags.LOGS_THAT_BURN)
+                .add(ModBlocks.ARAUCARIA_LOG.get().asItem())
+                .add(ModBlocks.STRIPPED_ARAUCARIA_LOG.get().asItem())
+                .add(ModBlocks.ARAUCARIA_WOOD.get().asItem())
+                .add(ModBlocks.STRIPPED_ARAUCARIA_WOOD.get().asItem());
+
+        this.tag(ItemTags.PLANKS)
+                .add(ModBlocks.ARAUCARIA_PLANKS.get().asItem());
+    }
+}
