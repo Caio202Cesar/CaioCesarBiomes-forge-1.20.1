60ef54374da35cced281750080b2afbcfd90bfb2
Caio202Cesar <caiodnobre@gmail.com>
2025-02-09 00:27:51 -0300

    Adding new biomes.

--- src/main/java/net/caiocesarmods/caiocesarbiomes/datagen/ModWorldGenProvider.java
+++ src/main/java/net/caiocesarmods/caiocesarbiomes/datagen/ModWorldGenProvider.java
@@ -2,6 +2,7 @@ package net.caiocesarmods.caiocesarbiomes.datagen;
 
 import net.caiocesarmods.caiocesarbiomes.CaioCesarBiomesMod;
 import net.caiocesarmods.caiocesarbiomes.worldgen.biome.ModBiomes.AutumnalPewenGroveBiome;
+import net.caiocesarmods.caiocesarbiomes.worldgen.biome.ModBiomes.TropicalRainforestBiome;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -13,9 +14,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(Registries.BIOME, AutumnalPewenGroveBiome::bootstrap);
-
-
+            .add(Registries.BIOME, AutumnalPewenGroveBiome::bootstrap)
+            .add(Registries.BIOME, TropicalRainforestBiome::bootstrap);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries, Set<String> modIds) {
         super(output, registries, BUILDER, Set.of(CaioCesarBiomesMod.MOD_ID));
