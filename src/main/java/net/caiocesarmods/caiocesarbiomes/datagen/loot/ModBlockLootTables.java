9e9cfe3c0eca7c9c7b7c04f638e3744a19909e66
Caio202Cesar <caiodnobre@gmail.com>
2025-02-13 15:57:20 -0300

    Adding new biomes.

--- src/main/java/net/caiocesarmods/caiocesarbiomes/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/caiocesarmods/caiocesarbiomes/datagen/loot/ModBlockLootTables.java
@@ -28,13 +28,15 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.add(ModBlocks.MONKEY_PUZZLE_LEAVES.get(), block ->
                 createLeavesDrops(block, ModBlocks.MONKEY_PUZZLE_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));
 
-        /*this.dropSelf(ModBlocks.NOTHOFAGUS_LOG.get());
+        this.dropSelf(ModBlocks.NOTHOFAGUS_LOG.get());
         this.dropSelf(ModBlocks.NOTHOFAGUS_WOOD.get());
         this.dropSelf(ModBlocks.STRIPPED_NOTHOFAGUS_LOG.get());
         this.dropSelf(ModBlocks.STRIPPED_NOTHOFAGUS_WOOD.get());
         this.dropSelf(ModBlocks.NOTHOFAGUS_PLANKS.get());
+        this.dropSelf(ModBlocks.CRIMSON_SOUTHERN_BEECH_SAPLING.get());
         this.add(ModBlocks.CRIMSON_SOUTHERN_BEECH_AUTUMNAL_LEAVES.get(), block ->
                 createLeavesDrops(block, ModBlocks.CRIMSON_SOUTHERN_BEECH_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));
+        this.dropSelf(ModBlocks.ORANGE_SOUTHERN_BEECH_SAPLING.get());
         this.add(ModBlocks.ORANGE_SOUTHERN_BEECH_AUTUMNAL_LEAVES.get(), block ->
                 createLeavesDrops(block, ModBlocks.ORANGE_SOUTHERN_BEECH_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));
 
@@ -43,6 +45,7 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.dropSelf(ModBlocks.STRIPPED_ITALIAN_CYPRESS_LOG.get());
         this.dropSelf(ModBlocks.STRIPPED_ITALIAN_CYPRESS_WOOD.get());
         this.dropSelf(ModBlocks.ITALIAN_CYPRESS_PLANKS.get());
+        this.dropSelf(ModBlocks.ITALIAN_CYPRESS_SAPLING.get());
         this.add(ModBlocks.ITALIAN_CYPRESS_LEAVES.get(), block ->
                 createLeavesDrops(block, ModBlocks.ITALIAN_CYPRESS_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));
 
@@ -51,16 +54,18 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.dropSelf(ModBlocks.STRIPPED_STONE_PINE_LOG.get());
         this.dropSelf(ModBlocks.STRIPPED_STONE_PINE_WOOD.get());
         this.dropSelf(ModBlocks.STONE_PINE_PLANKS.get());
+        this.dropSelf(ModBlocks.STONE_PINE_SAPLING.get());
         this.add(ModBlocks.STONE_PINE_LEAVES.get(), block ->
                 createLeavesDrops(block, ModBlocks.STONE_PINE_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));*/
 
-        //this.dropSelf(ModBlocks.FIG_LOG.get());
+        this.dropSelf(ModBlocks.FIG_LOG.get());
         this.dropSelf(ModBlocks.JUNGLE_FIG_LOG.get());
-        //this.dropSelf(ModBlocks.FIG_WOOD.get());
+        this.dropSelf(ModBlocks.FIG_WOOD.get());
         this.dropSelf(ModBlocks.JUNGLE_FIG_WOOD.get());
         this.dropSelf(ModBlocks.STRIPPED_FIG_LOG.get());
         this.dropSelf(ModBlocks.STRIPPED_FIG_WOOD.get());
         this.dropSelf(ModBlocks.FIG_PLANKS.get());
+        this.dropSelf(ModBlocks.FIG_SAPLING.get());
         this.dropSelf(ModBlocks.WEEPING_FIG_SAPLING.get());
         this.dropSelf(ModBlocks.INDIAN_LAUREL_SAPLING.get());
         this.add(ModBlocks.WEEPING_FIG_LEAVES.get(), block ->
